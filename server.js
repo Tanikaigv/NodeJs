4b65be3190ce76f0b7b6bca6b1e6c189b4773912
Tanikaigv <151662035+Tanikaigv@users.noreply.github.com>
2024-07-15 10:28:49 +0000

    Updated

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const http = require("http");
 const port = 8081;
 
+// HTTP methods:
+// >> GET
+// >> POST
+// >> DELETE
+// >> PATCH
+// >> PUT
+
+const student = ["name", "age", "dept"];
 http.createServer((req,res)=>{
-    res.writeHead(200, {"Content-Type":"text/html"});
-    res.write("<h2>Hey server started running in this port</h2>");
-    res.end();
+
+    const {method,url} = req
+    if (url === "/todos"){
+        if (method === "GET"){
+            res.writeHead(200);
+            res.write(student.toString());
+            res.end();
+        }
+    }
+
+    // res.writeHead(200, {"Content-Type":"text/html"});
+    // res.write("<h2>Hey server started running in this port</h2>");
+    // res.end();
 })
 .listen(port, () => {
     console.log(`Server running on port ${port}`);
